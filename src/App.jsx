986418ad098b5a2133ff00d92d40eb184bbfca3c
Rizki Nugraha <nugraharizki80@gmail.com>
2025-02-06 22:47:09 +0700

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 const App = () => {
   return (
-    <div className='flex h-screen items-center justify-center bg-gradient-to-r from-green-400 to-blue-500'>
-      <h1 className='text-4xl font-bold text-amber-500 font-bold'>
-        Hello Vite + React + Tailwind CSS!
-      </h1>
+    <div className='flex h-screen items-center justify-center bg-gradient-to-r from-orange-400 to-blue-500'>
+      <div className="bg-teal-400 p-4 rounded-lg shadow-lg">
+        <h1 className='text-4xl font-bold text-black-500 font-bold'>
+          Hello Vite + React + Tailwind CSS!
+        </h1>
+      </div>
     </div>
   )
 }
